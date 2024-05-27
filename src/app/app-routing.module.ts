fd11e6afd569af7eb85ed5706f31dd3ae5a79a72
ThalesRoch <thales.marcelino@gmail.com>
2024-05-27 20:26:00 -0300

    Creation pages module and module organization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
   },
   {
     path: '',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+//Modules
+import { PagesModule } from './pages/pages.module';
+
+//Components
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    PagesModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
