8cdbe44979e41d332666595244689621d6159c42
ThalesRoch <thales.marcelino@gmail.com>
2024-05-27 22:00:04 -0300

    feat: Creating PokeSearchComponts search logic

--- src/app/shared/poke-search/poke-search.component.ts
+++ src/app/shared/poke-search/poke-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-poke-search',
@@ -6,8 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./poke-search.component.scss'],
 })
 export class PokeSearchComponent {
+  @Output() public emmitSearch: EventEmitter<string> = new EventEmitter();
 
   constructor() { }
+
+  public search(value: string){
+    this.emmitSearch.emit(value);
+  }
 
 
 
